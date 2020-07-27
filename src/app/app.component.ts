ef4132ec8675ff27ea96fd86eb22d77975634c7e
Jakub <jswierczek213@gmail.com>
2020-07-27 18:09:35 +0200

    last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,16 @@ import { BreakpointObserver } from '@angular/cdk/layout';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements DoCheck {
+
   constructor(private breakpointObserver: BreakpointObserver) {}
 
-  isSmallScreen: boolean;
   links = [
     {title: 'Strona główna', link: '/'},
     {title: 'Filmy i seriale', link: '/movies-and-tv'},
     {title: 'Aktorzy', link: '/persons'}
   ];
+
+  isSmallScreen: boolean;
 
   ngDoCheck() {
     this.isSmallScreen = this.breakpointObserver.isMatched('(max-width: 768px)');
